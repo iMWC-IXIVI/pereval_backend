a80fa0136c119c76ff459643829b8951adb32b53
Andrey <v.andrei.dmitrievich@gmail.com>
2025-03-14 23:48:55 +0300

    added pereval create

--- app/api/v1/routers.py
+++ app/api/v1/routers.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.schemas.form_data import PerevalBaseFD, ImageBaseFD
-from db.crud import user_create, coord_create, level_create, image_create
+from db.crud import user_create, coord_create, level_create, image_create, pereval_create
 from db.session import get_db
 
 
@@ -12,14 +12,11 @@ router = APIRouter(prefix='/v1')
 
 @router.post('/create/')
 async def create(db: AsyncSession = Depends(get_db), data: PerevalBaseFD = Depends()):
-    # user = await user_create(db=db, user_data=data.user)
-    # coord = await coord_create(db=db, coord_data=data.coord)
-    # level = await level_create(db=db, level_data=data.level)
-
-    list_image = []
-    for image, title in zip(data.image_data, data.image_title):
-        list_image.append(ImageBaseFD(data=image, title=title))
-
-    image = await image_create(db=db, image_data=tuple(list_image))
+    # list_image = []
+    # for image, title in zip(data.image_data, data.image_title):
+    #     list_image.append(ImageBaseFD(data=image, title=title))
+    #
+    # image = await image_create(db=db, image_data=tuple(list_image))
+    pereval = await pereval_create(db=db, pereval_data=data)
 
     return {'message': 'success'}
--- app/db/crud/__init__.py
+++ app/db/crud/__init__.py
@@ -2,3 +2,4 @@ from .user import user_create
 from .coord import coord_create
 from .level import level_create
 from .image import image_create
+from .pereval import pereval_create
