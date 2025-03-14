4ef32c49d3760bd609042222306076d0ccbef56b
Andrey <v.andrei.dmitrievich@gmail.com>
2025-03-15 00:01:08 +0300

    fix bug with user create

--- app/db/crud/user.py
+++ app/db/crud/user.py
@@ -1,5 +1,7 @@
+from sqlalchemy import or_
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.future import select
 
 from fastapi import HTTPException
 
@@ -7,7 +9,18 @@ from db.schemas.form_data import UserBaseFD
 from db.models import User
 
 
+async def user_query(db: AsyncSession, user_data: UserBaseFD):
+    query = select(User).filter(or_(User.email == user_data.email, User.phone == user_data.phone))
+    result = await db.execute(query)
+
+    return result.fetchone()
+
+
 async def user_create(db: AsyncSession, user_data: UserBaseFD):
+    old_user = await user_query(db=db, user_data=user_data)
+    if old_user:
+        return old_user[0]
+
     user = User(
         fam=user_data.fam,
         name=user_data.name,
