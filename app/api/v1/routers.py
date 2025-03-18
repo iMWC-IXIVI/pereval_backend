1f2acac7f55ae17b06c5b45719bd9e4783aabc44
Andrey <v.andrei.dmitrievich@gmail.com>
2025-03-18 09:58:04 +0300

    try-except update pereval

--- app/api/v1/routers.py
+++ app/api/v1/routers.py
@@ -37,7 +37,7 @@ async def submit_data_create(db: AsyncSession = Depends(get_db), data: PerevalBa
     return {'detail': 'success'}
 
 
-@router.get('/{pk}', response_model=PerevalRead)
+@router.get('/{pk}', response_model=PerevalRead, status_code=200)
 async def submit_data_detail(pk: int = Path(..., title='Primary key', ge=1), db: AsyncSession = Depends(get_db)):
     try:
         query = select(Pereval).where(Pereval.id == pk).options(
@@ -59,7 +59,7 @@ async def submit_data_detail(pk: int = Path(..., title='Primary key', ge=1), db:
     return pereval_dict
 
 
-@router.get('', response_model=List[PerevalRead])
+@router.get('', response_model=List[PerevalRead], status_code=200)
 async def get_user(user_email: str = Query(..., title='User email'), db: AsyncSession = Depends(get_db)):
     try:
         query = select(Pereval).join(Pereval.user).where(User.email == user_email).options(
@@ -84,63 +84,66 @@ async def get_user(user_email: str = Query(..., title='User email'), db: AsyncSe
     return result_list
 
 
-@router.patch('/{pk}', response_model=dict)
+@router.patch('/{pk}', response_model=dict, status_code=200)
 async def submit_data_patch(
         pk: int = Path(..., title='Pereval update (patch)', ge=1),
         db: AsyncSession = Depends(get_db),
         pereval_data: PerevalUpdateFD = Depends()
 ):
-    query = select(Pereval).where(Pereval.id == pk).options(
-        selectinload(Pereval.user),
-        selectinload(Pereval.coord),
-        selectinload(Pereval.level),
-        selectinload(Pereval.images_pereval).selectinload(ImagePereval.image)
-    )
-    result = await db.execute(query)
-    pereval: Pereval = result.scalar_one_or_none()
+    try:
+        query = select(Pereval).where(Pereval.id == pk).options(
+            selectinload(Pereval.user),
+            selectinload(Pereval.coord),
+            selectinload(Pereval.level),
+            selectinload(Pereval.images_pereval).selectinload(ImagePereval.image)
+        )
+        result = await db.execute(query)
+        pereval: Pereval = result.scalar_one_or_none()
 
-    coord: Coord = pereval.coord
-    level: Level = pereval.level
+        coord: Coord = pereval.coord
+        level: Level = pereval.level
 
-    pereval.beauty_title = pereval_data.beauty_title
-    pereval.title = pereval_data.title
-    pereval.other_title = pereval_data.other_title
-    pereval.connect = pereval_data.connect
+        pereval.beauty_title = pereval_data.beauty_title
+        pereval.title = pereval_data.title
+        pereval.other_title = pereval_data.other_title
+        pereval.connect = pereval_data.connect
 
-    coord.latitude = pereval_data.coord.latitude
-    coord.longitude = pereval_data.coord.longitude
-    coord.height = pereval_data.coord.height
+        coord.latitude = pereval_data.coord.latitude
+        coord.longitude = pereval_data.coord.longitude
+        coord.height = pereval_data.coord.height
 
-    level.winter = pereval_data.level.winter
-    level.summer = pereval_data.level.summer
-    level.autumn = pereval_data.level.autumn
-    level.spring = pereval_data.level.spring
+        level.winter = pereval_data.level.winter
+        level.summer = pereval_data.level.summer
+        level.autumn = pereval_data.level.autumn
+        level.spring = pereval_data.level.spring
 
-    images = []
-    for image, title in zip(pereval_data.image_data, pereval_data.image_title):
-        images.append(ImageBaseFD(data=image, title=title))
+        images = []
+        for image, title in zip(pereval_data.image_data, pereval_data.image_title):
+            images.append(ImageBaseFD(data=image, title=title))
 
-    for data, instance in zip(images, pereval.images_pereval):
-        instance_file_path = os.path.join(settings.MEDIA_DIR, instance.image.data)
+        for data, instance in zip(images, pereval.images_pereval):
+            instance_file_path = os.path.join(settings.MEDIA_DIR, instance.image.data)
 
-        image = data.data
-        title = data.title
+            image = data.data
+            title = data.title
 
-        image.filename = f'{title}.{image.filename.split(".")[-1]}'
-        file_path = os.path.join(settings.MEDIA_DIR, image.filename)
-
-        if os.path.exists(file_path):
-            image.filename = f'{settings.generate_unique_value()}{image.filename}'
+            image.filename = f'{title}.{image.filename.split(".")[-1]}'
             file_path = os.path.join(settings.MEDIA_DIR, image.filename)
 
-        with open(file_path, 'wb+') as file:
-            file.write(await image.read())
+            if os.path.exists(file_path):
+                image.filename = f'{settings.generate_unique_value()}{image.filename}'
+                file_path = os.path.join(settings.MEDIA_DIR, image.filename)
 
-        instance.image.data = image.filename
-        instance.image.title = title
+            with open(file_path, 'wb+') as file:
+                file.write(await image.read())
 
-        os.remove(instance_file_path)
+            instance.image.data = image.filename
+            instance.image.title = title
 
-    await db.commit()
+            os.remove(instance_file_path)
+
+        await db.commit()
+    except Exception as e:
+        return JSONResponse(content={'error': 'can\'t update pereval'}, status_code=400)
 
     return {'message': 'success'}
