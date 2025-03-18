fe0ca49f1f59fc9afd4b560e7a9ecbdd11e132d3
Andrey <v.andrei.dmitrievich@gmail.com>
2025-03-18 09:17:02 +0300

    try-except post submit data

--- app/api/v1/routers.py
+++ app/api/v1/routers.py
@@ -3,6 +3,7 @@ import os
 from typing import List
 
 from fastapi import APIRouter, Depends, Path, Query
+from fastapi.responses import JSONResponse
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
@@ -14,22 +15,26 @@ from db.schemas.form_data import PerevalBaseFD, ImageBaseFD, PerevalUpdateFD
 from db.schemas.json import PerevalRead, ImageRead
 from db.crud import image_create, pereval_create, image_pereval_create
 from db.session import get_db
-from db.models import Pereval, ImagePereval, User, Level, Coord, Image
+from db.models import Pereval, ImagePereval, User, Level, Coord
 
 
 router = APIRouter(prefix='/submitData')
 
 
-@router.post('/', response_model=dict)
+@router.post('/', response_model=dict, status_code=201)
 async def submit_data_create(db: AsyncSession = Depends(get_db), data: PerevalBaseFD = Depends()):
-    list_image = []
-    for image, title in zip(data.image_data, data.image_title):
-        list_image.append(ImageBaseFD(data=image, title=title))
+    try:
+        list_image = []
+        for image, title in zip(data.image_data, data.image_title):
+            list_image.append(ImageBaseFD(data=image, title=title))
 
-    image = await image_create(db=db, image_data=tuple(list_image))
-    pereval = await pereval_create(db=db, pereval_data=data)
-    await image_pereval_create(db=db, image_data=tuple(image), pereval_data=pereval)
-    return {'message': 'success'}
+        image = await image_create(db=db, image_data=tuple(list_image))
+        pereval = await pereval_create(db=db, pereval_data=data)
+        await image_pereval_create(db=db, image_data=tuple(image), pereval_data=pereval)
+    except Exception as e:
+        return JSONResponse(content={'error': 'can"t create pereval'}, status_code=400)
+
+    return {'detail': 'success'}
 
 
 @router.get('/{pk}', response_model=PerevalRead)
